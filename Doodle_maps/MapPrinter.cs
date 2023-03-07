0b0615ee7ecba91f9b5ebeb2516a20282912b41e
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-03-07 20:56:57 +0200

    First point prints as 'A'

---
+++ Doodle_maps/MapPrinter.cs
@@ -0,0 +1,51 @@
+namespace Kse.Algorithms.Samples
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MapPrinter
+    {
+        public void Print(string[,] maze, List<Point> path)
+        {
+            PrintTopLine();
+
+            var start = path[0];
+            var goal = path[^1];
+
+            for (var row = 0; row < maze.GetLength(1); row++)
+            {
+                Console.Write($"{row}\t");
+                for (var column = 0; column < maze.GetLength(0); column++)
+                {
+                    if (start.Equals(new Point(row: row, column: column)))
+                    {
+                        Console.Write("A");
+                    }
+                    else
+                    {
+                        Console.Write(maze[column, row]);
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            void PrintTopLine()
+            {
+                Console.Write($" \t");
+                for (int i = 0; i < maze.GetLength(0); i++)
+                {
+                    Console.Write(i % 10 == 0? i / 10 : " ");
+                }
+    
+                Console.Write($"\n \t");
+                for (int i = 0; i < maze.GetLength(0); i++)
+                {
+                    Console.Write(i % 10);
+                }
+    
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
---
+++ Doodle_maps/Program.cs
@@ -0,0 +1,28 @@
+﻿using Kse.Algorithms.Samples;
+
+var generator = new MapGenerator(new MapGeneratorOptions()
+{
+    Height = 10,
+    Width = 15,
+    Seed = 3,
+    Noise= 0.1f,
+});
+
+string[,] map = generator.Generate();
+
+var start = new Point(row: 2, column: 0);
+var goal = new Point(row: 8, column: 14);
+
+var my_result = GetShortestPath(map, start, goal);
+
+
+
+List<Point> GetShortestPath(string[,] map, Point start, Point goal)
+{
+    List<Point> result = new List<Point>();
+    result.Add(start);
+
+    result.Add(goal);
+    new MapPrinter().Print(map, result);
+    return result;
+}
