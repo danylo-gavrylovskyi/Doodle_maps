12bcadcd641a3fcffdfbf4f82b93547c48faaaae
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-03-17 12:11:00 +0200

    Debuged path finding problems and added algorithms comparison

--- Doodle_maps/Program.cs
+++ Doodle_maps/Program.cs
@@ -5,52 +5,90 @@ bool IsEqual(Point a, Point b)
     return a.Column == b.Column && a.Row == b.Row;
 }
 
-//void PrintMapWithPath(string[,] map, List<Point> path, Point start, Point goal)
-//{
-//    foreach (Point p in path)
-//    {
-//        if (IsEqual(p, start))
-//        {
-//            map[p.Column, p.Row] = "A";
-//        }
-//        else if (IsEqual(p, goal))
-//        {
-//            map[p.Column, p.Row] = "B";
-//        }
-//        else
-//        {
-//            map[p.Column, p.Row] = ".";
-//        }
-//    }
-
-//    new MapPrinter().Print(map);
-//}
-
 var generator = new MapGenerator(new MapGeneratorOptions()
 {
-    Height = 10,
-    Width = 15,
-    Seed = 2,
+    Height = 40,
+    Width = 40,
+    Seed = 3,
     AddTraffic = true,
-    TrafficSeed = 1
+    TrafficSeed = 1,
+    Noise=0.25f
 });
 
 
 string[,] map = generator.Generate();
 var start = new Point(row: 0, column: 0);
-var goal = new Point(row: 6, column: 9);
+var goal = new Point(row: 38, column: 39);
+map[start.Column, start.Row] = " ";
+map[goal.Column, goal.Row] = " ";
+List<Point> Visited = new List<Point>();
 
-new MapPrinter().Print(map, new List<Point>(), start, start);
-List<Point> path = BFS(start, goal);
+new MapPrinter().Print(map, new List<Point>(), start, goal);
+List<Point> path = AStar(start, goal, Visited);
+Console.WriteLine($"\n Heuristic Visited {Visited.Count}\n");
+new MapPrinter().Print(map, path, start, goal);
+
+
+path = Dijkstra(start, goal, Visited);
+Console.WriteLine($"\n Dijkstra Visited {Visited.Count}\n");
 new MapPrinter().Print(map, path, start, goal);
 
 int Heuristic(Point current, Point goal)
 {
-    return Math.Abs(current.Row - goal.Row) + (current.Column - goal.Column);
+    return Math.Abs(current.Row - goal.Row) + Math.Abs(current.Column - goal.Column);
 }
 
-List<Point> BFS(Point start, Point goal)
+List<Point> AStar(Point start, Point goal, List<Point> Visited)
 {
+    Dictionary<Point, Point> origins = new Dictionary<Point, Point>();
+    var queue = new PriorityQueue<Point, int>();
+    queue.Enqueue(start, 0);
+    Point next = start;
+    int priority;
+    float time = 0f;
+
+    while (!IsEqual(next, goal) && queue.Count > 0)
+    {
+        var is_ok = queue.TryDequeue(out next, out priority);
+
+        foreach (Point neighbour in GetNeighbours(next.Row, next.Column, map))
+        {
+            Visit(neighbour);
+            if (!origins.TryGetValue(neighbour, out _))
+            {
+                int.TryParse(map[neighbour.Column, neighbour.Row], out int num);
+                origins.Add(neighbour, next);
+                queue.Enqueue(neighbour, priority + num + Heuristic(neighbour, goal));
+            }
+        }
+    }
+
+
+    List<Point> path = new List<Point>();
+    Point current = goal;
+
+    while (!IsEqual(current, start))
+    {
+        path.Add(current);
+        origins.TryGetValue(current, out current);
+        int.TryParse(map[current.Column, current.Row], out int num);
+        time += 1 / (60 - (num - 1) * 6f);
+    }
+
+    path.Add(start);
+    path.Reverse();
+    Console.WriteLine($"\nRoad time - {time}\n");
+    return path;
+}
+
+void Visit(Point point)
+{
+    Visited.Add(point);
+}
+
+List<Point> Dijkstra(Point start, Point goal, List<Point> Visited)
+{
+    Visited.Clear();
     Dictionary<Point, Point> origins = new Dictionary<Point, Point>();
     var queue = new PriorityQueue<Point, int>();
     queue.Enqueue(start, 0);
@@ -63,13 +101,12 @@ List<Point> BFS(Point start, Point goal)
         var is_ok = queue.TryDequeue(out next, out priority);
         foreach (Point neighbour in GetNeighbours(next.Row, next.Column, map))
         {
+            Visit(neighbour);
             if (!origins.TryGetValue(neighbour, out _))
             {
-                int num;
-                int.TryParse(map[neighbour.Column, neighbour.Row], out num);
+                int.TryParse(map[neighbour.Column, neighbour.Row], out int num);
                 origins.Add(neighbour, next);
-                queue.Enqueue(neighbour, priority + num + Heuristic(neighbour, goal));
-                time += 1 / (60 - (num - 1) * 6f);
+                queue.Enqueue(neighbour, priority + num);
             }
         }
     }
@@ -81,48 +118,14 @@ List<Point> BFS(Point start, Point goal)
     {
         path.Add(current);
         origins.TryGetValue(current, out current);
+        int.TryParse(map[current.Column, current.Row], out int num);
+        time += 1 / (60 - (num - 1) * 6f);
     }
 
     path.Add(start);
     path.Reverse();
-    Console.WriteLine($"\nRoad time - {time}\n");
     return path;
 }
-
-//List<Point> BFS(Point start, Point goal)
-//{
-//    Dictionary<Point, Point> origins = new Dictionary<Point, Point>();
-//    var queue = new PriorityQueue<Point, int>();
-//    queue.Enqueue(start, 0);
-//    Point next = start;
-//    int priority;
-
-//    while (!IsEqual(next, goal) && queue.Count > 0)
-//    {
-//        var is_ok = queue.TryDequeue(out next, out priority);
-//        foreach (Point neighbour in GetNeighbours(next.Row, next.Column, map))
-//        {
-//            if (!origins.TryGetValue(neighbour, out _))
-//            {
-//                origins.Add(neighbour, next);
-//                queue.Enqueue(neighbour, priority + 1);
-//            }
-//        }
-//    }
-
-//    List<Point> path = new List<Point>();
-//    Point current = goal;
-
-//    while (!IsEqual(current, start))
-//    {
-//        path.Add(current);
-//        origins.TryGetValue(current, out current);
-//    }
-
-//    path.Add(start);
-//    path.Reverse();
-//    return path;
-//}
 
 List<Point> GetNeighbours(int row, int column, string[,] maze)
 {
